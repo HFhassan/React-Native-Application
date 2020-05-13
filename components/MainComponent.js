a450a3f63f4f4a543447fa866b33dd59c8613d58
Hassan Farooqui <hassanfarooqui817@gmail.com>
2020-05-13 17:00:35 +0500

    Icon and Fonts

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,12 +2,20 @@ import React , { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { View, Text,  Platform, Image, StyleSheet, ScrollView } from 'react-native';
+import { createStackNavigator, createDrawerNavigator,DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+import Aboutus from './AboutusComponent';
+import Contact from './ContactComponent';
 
 const MenuNavigator = createStackNavigator({
-    Menu: { screen: Menu},
+    Menu: { screen: Menu,
+    navigationOptions: ({ navigation })=>({
+        headerLeft: <Icon name='menu' size={24}
+        color='white'
+        onPress= {( )=> navigation.toggleDrawer()}
+        />
+    }) },
     DishDetail: { screen: DishDetail }
 
 },
@@ -24,41 +32,152 @@ const MenuNavigator = createStackNavigator({
     }
 });
 const HomeNavigator = createStackNavigator({
-    Home: { screen: Home}
+    Home: { screen: Home }
 },
 {
 
-    navigationOptions:({ navigation }) => ({
+    navigationOptions:{
         headerStyle: {
             backgroundColor: '#512DA8'
         },
         headerTintColor: '#fff',
         headerTitleStyle:{
             color: '#fff'
+            // headerLeft: <Icon name='menu' size={24}
+            //     color='white'
+            //     onPress= {( )=> navigation.toggleDrawer()}
+            // />
+        }
+    
+}
+  }  );
+
+const AboutusNavigator = createStackNavigator({
+    Aboutus: { screen: Aboutus}
+},
+{
+    navigationOptions: ({ navigation }) => ({
+        headerStyle:{
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:{
+            color: '#fff'
+        //     headerLeft: <Icon name='menu' size={24}
+        //         color='white'
+        //         onPress= {( )=> navigation.toggleDrawer()}
+        // />
+        }
+    })
+}
+);
+
+const ContactNavigator = createStackNavigator({
+    Contact: {screen: Contact}
+},
+{
+    navigationOptions: ({ navigation }) => ({
+        headerStyle:{
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:{
+            color: '#fff'
+        //     headerLeft: <Icon name='address-card' size={24}
+        //         color='white'
+        //         onPress= {( )=> navigation.toggleDrawer()}
+        // />
         }
     })
 });
+
+const CustomDrawerContentComponent = (props) =>(
+    <ScrollView>
+        <SafeAreaView style={styles.container}
+            forceInset= {{top: 'always', horizontal: 'never'}}
+        >
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>
+                    <Image source={require('./images/logo.png')}
+                        style={styles.drawerImage} />
+                </View>
+                <View style={{flex:2}}>
+                    <Text style={styles.drawerHeaderText}>Restornate Con Fusion</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+);
 
 const MainNavigator  = createDrawerNavigator({
     Home:{
         screen: HomeNavigator,
         navigationOptions: {
             title: 'Home',
-            drawerLable: 'Home'
+            drawerLable: 'Home',
+            drawerIcon: ({ tintColor }) =>(
+                <Icon
+                    name='home'
+                    type='font-awesome'
+                    size={24}
+                    color={ tintColor }
+                    />
+            )
         }
     },
     Menu:{
         screen: MenuNavigator,
         navigationOptions: {
             title: 'Menu',
-            drawerLable: 'Menu'
-        },
+            drawerLable: 'Menu',
+            drawerIcon: ({ tintColor }) =>(
+                <Icon
+                    name='list'
+                    type='font-awesome'
+                    size={24}
+                    color={ tintColor }
+                    />
+            )
+        }
 
-    }
+    },
+    
+        Aboutus:{
+            screen: AboutusNavigator,
+            navigationOptions:{
+                title: 'About Us',
+                drawerLable: 'About Us',
+                drawerIcon: ({ tintColor }) =>(
+                    <Icon
+                        name='info-circle'
+                        type='font-awesome'
+                        size={24}
+                        color={ tintColor }
+                        />
+                )
+            }
+        },
+        Contact: {
+            screen: ContactNavigator,
+            navigationOptions:{
+                title: 'Contact Us',
+                drawerLable: 'Contact',
+                drawerIcon: ({ tintColor }) =>(
+                    <Icon
+                        name='address-card'
+                        type='font-awesome'
+                        size={22}
+                        color={ tintColor }
+                        />
+                )
+            }
+        }
+
 },
     {
-        drawerBackgroundColor: '#D1C4E9'
-    
+        drawerBackgroundColor: '#D1C4E9',
+        contentComponent: CustomDrawerContentComponent
 });
 
 class Main extends  Component {
@@ -71,5 +190,29 @@ class Main extends  Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    drawerHeader:{
+        backgroundColor: "#512DA8",
+        height: 140,
+        alignItems: 'center',
+        justifyContent: "center",
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText:{
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage:{
+        margin: 10,
+        width: 80,
+        height: 60
+    }
+})
 
 export default Main;
---
+++ shared/contact.js
@@ -0,0 +1,12 @@
+export const CONTACT = [
+{
+    title: 'Our Address',
+
+    street: '121, Clear Water Bay Road',
+    area: 'Clear Water Bay, Kowloon',
+    region: 'HONG KONG',
+    tel: 'Tel: +852 1234 5678',
+    fax: 'Fax: +852 8765 4321',
+    email: 'Email:confusion@food.net'
+}
+]
